f0feb4555d28d63efd955ead0ed9a927f4447f5d
Tintu Jose <tintjose@gmail.com>
2018-07-10 19:14:15 -0700

    second commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './main.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {createStore, applyMiddleware} from 'redux'
+import {composeWithDevTools} from 'redux-devtools-extension'
+import thunk from 'redux-thunk'
+import {browseHistory, Router, Route} from 'react-router'
+import {syncHistoryWithStore} from 'react-router-redux'
+import reducers from 'reducers'
+import {Provider} from 'react-redux'
+import Layout from 'containers/layout'
+import Phones from 'containers/phones'
 
-document.write('Hello React/Redux!')
+const store=createStore(reducers, composeWithDevTools(
+  applyMiddleware(thunk)
+))
+const history=syncHistoryWithStore(browseHistory, store)
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route component={Layout}>
+      <Route path='/' component={Phones} />
+      </Route>
+    </Router>
+  </Provider>,
+document.getElementById('root')
+)
